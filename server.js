0e9e594bb9550f3730516f0fcbd8c5d5e4b71d13
Aymane Hrouch <jo.aymane@gmail.com>
2022-09-02 01:29:23 +0100

    restore server.js, deleted by accident smh

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// sockets handlers
+const countdownHandler = require("./socketsHandlers/countdownHandler");
+const usersHandler = require("./socketsHandlers/usersHandler");
+const messagesHandler = require("./socketsHandlers/messagesHandler");
+
+const onConnection = socket => {
+  console.log("new connection");
+  usersHandler(io, socket);
+  countdownHandler(io, socket);
+  messagesHandler(io, socket);
+};
+
+io.on("connection", onConnection);
+
+// Server listening
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the server</h1>");
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server listening at ${PORT}...`));
